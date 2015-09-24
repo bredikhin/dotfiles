1bd0e528d6c1ffe71d62c44b7edfb4dc7e470750
Ruslan Bredikhin <rus.brn@gmail.com>
2015-09-24 00:42:10 -0400

    clean up

--- tmuxifier/editor.window.sh
+++ tmuxifier/editor.window.sh
@@ -1,13 +1,4 @@
-#window_root "~/Sites"
 new_window "editor"
-
-# Split window into panes
 split_v 20
-
 run_cmd "vim" 1
-
-# Paste text
-#send_keys "top"    # paste into active pane
-#send_keys "date" 1 # paste into pane 1
-
 select_pane 2
