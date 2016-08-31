bb91ca25fb26ecd90c9501e824ed45a301aba661
Ruslan Bredikhin <rus.brn@gmail.com>
2016-08-30 22:13:12 -0400

    add more tmuxifier session fixes

--- tmuxifier/default.session.sh
+++ tmuxifier/default.session.sh
@@ -1,10 +1,12 @@
 session_root "~/Projects"
 if initialize_session "default"; then
   new_window "default"
+  split_v 50
+  select_pane 1
   split_h 50
-  split_v 50
-  select_pane 2
-  split_v 50
+  run_cmd "sudo htop"
+  select_pane 3
+  split_h 50
   select_pane 1
 fi
 
--- tmuxifier/lb/lblogs.window.sh
+++ tmuxifier/lb/lblogs.window.sh
@@ -1,6 +1,6 @@
 new_window "lblogs"
 window_root "~/Projects/ct/lb"
-split_v 50
 run_cmd "tail -f ./lb/app/tmp/logs/error.log"
+split_v 50
 select_pane 2
 run_cmd "tail -f ./lbapi/app/log/app.log"
